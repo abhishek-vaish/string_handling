bcc0c65540b986315404807f8b3db151838ad0b4
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-02 23:42:24 +0530

    Create Scanner object.

--- src/solution6/Solution6.java
+++ src/solution6/Solution6.java
@@ -1,8 +1,11 @@
 package solution6;
 
+import java.util.Scanner;
+
 public class Solution6 {
     public static void main(String[] args) {
         String str;
+        Scanner scan = new Scanner(System.in);
 
     }
 }
