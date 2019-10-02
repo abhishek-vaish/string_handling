7983ef2bbe5b83a94dc2224813b80389d7f76ab1
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-02 22:54:50 +0530

    Create Scanner object.

--- src/solution2/Solution2.java
+++ src/solution2/Solution2.java
@@ -1,9 +1,12 @@
 package solution2;
 
+import java.util.Scanner;
+
 public class Solution2 {
     public static void main(String[] args) {
         String str;
         int index;
+        Scanner scan = new Scanner(System.in);
 
 
     }
