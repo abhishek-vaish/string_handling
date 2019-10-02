b69118483247e78191c2863ab2b8c1a16720ce81
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-02 23:05:56 +0530

    Create Scanner Method Object.

--- src/solution3/Solution3.java
+++ src/solution3/Solution3.java
@@ -1,9 +1,12 @@
 package solution3;
 
+import java.util.Scanner;
+
 public class Solution3 {
     public static void main(String[] args) {
         String str;
         char ch;
+        Scanner scan = new Scanner(System.in);
 
     }
 
