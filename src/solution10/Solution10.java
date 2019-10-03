7d27179b08a505d90556a385e8a4af7d83365fb2
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-03 22:57:41 +0530

    Declare Scanner class, create object and take user input

--- src/solution10/Solution10.java
+++ src/solution10/Solution10.java
@@ -1,9 +1,14 @@
 package solution10;
 
+import java.util.Scanner;
+
 public class Solution10 {
     public static void main(String[] args) {
         String str;
         char ch;
+        Scanner scan = new Scanner(System.in);
+        str = scan.nextLine();
+        ch = scan.nextLine().charAt(0);
 
     }
 }
