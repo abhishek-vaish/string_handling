ee325c5b60dbab8c5c1da8605056e41981d8b903
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-03 23:08:17 +0530

    Declare all variables.

--- src/solution7/Solution7.java
+++ src/solution7/Solution7.java
@@ -1,21 +1,11 @@
 package solution7;
 
-import java.util.Scanner;
-
 public class Solution7 {
     public static void main(String[] args) {
         String str;
         int weight = 0;
         int ascii;
         char ch;
-        Scanner scan = new Scanner(System.in);
-        str = scan.nextLine();
-        int len = str.length();
-        for (int i = 0; i < len; i++) {
-            ch = str.charAt(i);
-            ascii = ch;
-            weight = weight + ascii;
-        }
-        System.out.println("totalWeight = " + weight);
+
     }
 }
