b8ba73afea577463957deb17305d883af5d6efb4
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-03 23:10:38 +0530

    Declare Scanner class, create object, take user input and find the length of the string

--- src/solution7/Solution7.java
+++ src/solution7/Solution7.java
@@ -1,11 +1,16 @@
 package solution7;
 
+import java.util.Scanner;
+
 public class Solution7 {
     public static void main(String[] args) {
         String str;
         int weight = 0;
         int ascii;
         char ch;
+        Scanner scan = new Scanner(System.in);
+        str = scan.nextLine();
+        int len = str.length();
 
     }
 }
