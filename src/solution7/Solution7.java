abecb4915edafc71265543bb614297191f095faa
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-03 21:40:04 +0530

    Declare all variables.

--- src/solution7/Solution7.java
+++ src/solution7/Solution7.java
@@ -2,5 +2,10 @@ package solution7;
 
 public class Solution7 {
     public static void main(String[] args) {
+        String str;
+        int weight = 0;
+        int ascii;
+        char ch;
+
     }
 }
