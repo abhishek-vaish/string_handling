8e6fd624e773c16876785f8a5bb05b0d0ede0605
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-03 23:01:19 +0530

    Declare Variables

--- src/solution9/Solution9.java
+++ src/solution9/Solution9.java
@@ -2,6 +2,11 @@ package solution9;
 
 public class Solution9 {
     public static void main(String[] args) {
+        String str;
+        char ch;
+        int upperCase = 0;
+        int lowerCase = 0;
+        int ascii = 0;
 
     }
 }
