d36d8027fce7d9b8b2fcddd57178cc53b079adde
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-03 22:58:46 +0530

    Take index of the char input by the user

--- src/solution10/Solution10.java
+++ src/solution10/Solution10.java
@@ -9,6 +9,7 @@ public class Solution10 {
         Scanner scan = new Scanner(System.in);
         str = scan.nextLine();
         ch = scan.nextLine().charAt(0);
+        int index = str.indexOf(ch);
 
     }
 }
