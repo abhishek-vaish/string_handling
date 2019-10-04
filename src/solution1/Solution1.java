68540f48f81fdd0b8da7c0dc2f30ea71cd4c2004
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-10-04 21:50:34 +0530

    Declare Scanner class, take user input.

--- src/solution1/Solution1.java
+++ src/solution1/Solution1.java
@@ -1,9 +1,15 @@
 package solution1;
 
+import java.util.Scanner;
+
 public class Solution1 {
     public static void main(String[] args) {
         int number;
         String str;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number string you want to input :");
+        number = scan.nextInt();
+        scan.nextLine();
 
 
     }
